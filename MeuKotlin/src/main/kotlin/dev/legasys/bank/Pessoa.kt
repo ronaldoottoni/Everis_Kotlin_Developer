85ee1dfdd9022edbd86fb7407eb0684bb31f5651
ronaldoottoni <Rtue5fy7@>
2021-03-01 16:50:53 -0300

    Classe pessoa foi adicionado o private set no campo CPF para que o mesmo nao possa ser alterado em tempo de execução

--- MeuKotlin/src/main/kotlin/dev/legasys/bank/Pessoa.kt
+++ MeuKotlin/src/main/kotlin/dev/legasys/bank/Pessoa.kt
@@ -2,12 +2,18 @@ package dev.legasys.bank
 
 class Pessoa {
     var nome: String = "Ronaldo"
+
     var cpf: String = "123.123.123-12"
+    private set
+
 }
 
 fun main(){
     val ronaldo = Pessoa()
 
+    ronaldo.cpf = "159.157.154-35"
+
     println(ronaldo.nome)
     println(ronaldo.cpf)
+
 }
