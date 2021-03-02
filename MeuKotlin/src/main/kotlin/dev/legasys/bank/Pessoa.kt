8e682ab6f7cd306ab68b9e78918211e82aa79404
ronaldoottoni <Rtue5fy7@>
2021-03-02 16:58:32 -0300

    Added a constructor and a function that print NAME and CPF together.

--- MeuKotlin/src/main/kotlin/dev/legasys/bank/Pessoa.kt
+++ MeuKotlin/src/main/kotlin/dev/legasys/bank/Pessoa.kt
@@ -6,12 +6,20 @@ class Pessoa {
     var cpf: String = "123.123.123-12"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val ronaldo = Pessoa()
 
+    /*
+    println(ronaldo)
     println(ronaldo.nome)
     println(ronaldo.cpf)
+    */
+
+    println(ronaldo.pessoaInfo())
 
 }
