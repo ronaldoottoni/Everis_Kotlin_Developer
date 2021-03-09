f9b7af215b42b43173294c22fad5b7855c0344bb
ronaldoottoni <Rtue5fy7@>
2021-03-09 11:20:52 -0300

    In TesteOperacoes was added and learned about lambda expressions using FILTER, COUNT, FUND and ANY

--- api-collections/src/main/kotlin/com/legasys/collection/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/com/legasys/collection/TesteOperacoes.kt
@@ -15,9 +15,18 @@ fun main() {
 
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
 
-    println("*------ > 2500 ---------*")
+    println("*------ filter > 2500 ---------*")
 
     salariosMaiorQue2500.forEach { println(it)}
 
+    println("*------ 2000 <count> 5000 ---------*")
+    println(salarios.count { it in 2000.0..5000.0 })
 
+    println("*------ <find> ---------*")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("*------ <any> ---------*")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })
 }
