5fbb3a6830bebd2ea4b733e9c1031ebddc015bd7
Ronaldo Ottoni Borges de Carvalho <ronaldoottoni@gmail.com>
2021-03-18 16:13:11 -0300

    Add some overrides to show by log the app step's (onCreate, onStart, onResume ...)

--- imcapp/app/src/main/java/com/legacys/imcapp/MainActivity.kt
+++ imcapp/app/src/main/java/com/legacys/imcapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.legacys.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w("lifecycle", "entrei no onCreate")
+        //finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle", "entrei no onSTART")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifeCycle", "entrei no onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifeCycle", "entrei no onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("lifeCycle", "entrei no onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("lifeCycle", "entrei no onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("lifeCycle", "entrei no onDestroy")
     }
 }
