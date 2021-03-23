4a2965391eb6e937fc970715ee6b46c279d662b4
Ronaldo Ottoni Borges de Carvalho <ronaldoottoni@gmail.com>
2021-03-23 12:25:59 -0300

    First Run And set setListeners to TextView and Button to calculate the IMC and show it in the TextView. App Finished and running.

--- imcapp/app/src/main/java/com/legacys/imcapp/MainActivity.kt
+++ imcapp/app/src/main/java/com/legacys/imcapp/MainActivity.kt
@@ -1,43 +1,39 @@
 package com.legacys.imcapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.w("lifecycle", "entrei no onCreate")
+        setListeners()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.w("lifecycle", "entrei no onSTART")
+    private fun setListeners(){
+        alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        pesoEDT?.doOnTextChanged{text, _, _, _ ->
+            titleTXT?.text = text
+        }
+
+        calcularBTN?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.w("lifeCycle", "entrei no onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.w("lifeCycle", "entrei no onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.w("lifeCycle", "entrei no onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("lifeCycle", "entrei no onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.w("lifeCycle", "entrei no onDestroy")
+    private fun calcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null){
+            val imc = peso / (altura * altura)
+            titleTXT.text = "seu IMC é: \n%.2f ".format(imc)
+        }
     }
 }
